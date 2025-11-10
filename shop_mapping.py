e6260d54cb41afd31ddf299711deb3c827c3fe4e
pfm-mvp <christiaan.vanrooijen@pfm-intelligence.com>
2025-11-10 15:39:47 +0100

    Add files via upload

---
+++ shop_mapping.py
@@ -0,0 +1,12 @@
+# shop_mapping.py
+# ✅ Enkelvoudige bron: id → {name, region}
+SHOP_NAME_MAP = {
+    29658: {"name": "Amsterdam", "region": "Noord NL", "postcode": "3811"},
+    29679: {"name": "Apeldoorn",  "region": "Noord NL", "postcode": "7331"},
+    29683: {"name": "Den Haag",  "region": "Zuid NL",  "postcode": "2511"},
+    29669: {"name": "Gouda",    "region": "Noord NL", "postcode": "2801"},
+    29771: {"name": "Haarlem",     "region": "Noord NL", "postcode": "2011"},
+    29770: {"name": "Leiden", "region": "Zuid NL",  "postcode": "2311"},
+    28704: {"name": "Rotterdam",  "region": "Zuid NL",  "postcode": "3087"},
+    29691: {"name": "Tilburg",      "region": "Zuid NL",  "postcode": "5038"},
+}
