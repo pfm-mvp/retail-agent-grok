f1cde2bfa1cc34f0f5d7607a6a7e19a38f38dc6b
pfm-mvp <christiaan.vanrooijen@pfm-intelligence.com>
2025-11-11 13:35:05 +0100

    Update __init__.py

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,1 +1,8 @@
+# helpers/__init__.py
+from .ui import inject_css, kpi_card, brand_colors
+from .normalize import normalize_vemcount_response, to_wide
 
+__all__ = [
+    "inject_css", "kpi_card", "brand_colors",
+    "normalize_vemcount_response", "to_wide"
+]
