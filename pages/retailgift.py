7fb535c79d143af18e6f09475bfdeadb9ea08aab
pfm-mvp <christiaan.vanrooijen@pfm-intelligence.com>
2025-11-11 13:39:08 +0100

    Update retailgift.py

--- pages/retailgift.py
+++ pages/retailgift.py
@@ -1,7 +1,3 @@
-# pages/retailgift.py – VOEG DIT TOE OP REGEL 1
-import os
-st.write("DEBUG: helpers dir:", os.listdir("../helpers"))  # <-- FORCEER CHECK
-
 # pages/retailgift.py – 100% WERKT MET helpers/ IN ROOT
 import streamlit as st
 import requests
